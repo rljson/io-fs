c8b2cbfeeaff8a68a655d3d21136f53292fa96d0
Jan Balzer <154504062+Jan-Balzer@users.noreply.github.com>
2026-09-17 16:07:18 +0200

    Add the rljson DNA (#12)
    * #gg: generated DNA
    
    * #gg: generated DNA
    
    * Add the rljson DNA
    
    * #gg: Add .gg/gg.json check results
    
    * Update to import.meta.dirname
    
    * #gg: Add .gg/gg.json check results
    
    * update pipeline
    
    * #gg: Add .gg/gg.json check results
    
    * update pipeline again
    
    * #gg: Add .gg/gg.json check results

--- scripts/functions/colors.js
+++ scripts/functions/colors.js
@@ -1,6 +1,6 @@
-/*
+/**
  * @license
- * Copyright (c) 2025 Rljson
+ * Copyright (c) rljson
  *
  * Use of this source code is governed by terms that can be
  * found in the LICENSE file in the root of this package.
@@ -22,3 +22,6 @@ export const bgMagenta = (str) => `\x1b[45m${str}\x1b[0m`;
 export const bgCyan = (str) => `\x1b[46m${str}\x1b[0m`;
 export const bgWhite = (str) => `\x1b[47m${str}\x1b[0m`;
 export const bgBlack = (str) => `\x1b[40m${str}\x1b[0m`;
+export const gray = (str) => `\x1b[90m${str}\x1b[0m`;
+export const lightGray = (str) => `\x1b[37m${str}\x1b[0m`;
+export const reset = (str) => `\x1b[0m${str}\x1b[0m`;
